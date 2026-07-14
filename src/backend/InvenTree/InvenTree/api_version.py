8d84b1ca31307980e71b863d611705e131107054
Oliver <oliver.henry.walters@gmail.com>
2026-07-14 14:34:17 +1000

    [feature] Unique parameters (#12388)
    * Add uniqueness requirement for parameter values
    
    * Add documentation
    
    * Do not copy unique parameters
    
    * Updated playwright tests
    
    * Enhanced validation checks for uniqueness
    
    * Update documentation
    
    * Update CHANGELOG
    
    * Bump API version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,10 +1,20 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 519
+INVENTREE_API_VERSION = 522
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v522 -> 2026-07-14 : https://github.com/inventree/InvenTree/pull/12388
+    - Adds "unique" field to the ParameterTemplate model
+
+v521 -> 2026-07-12 : https://github.com/inventree/InvenTree/pull/12360
+    - Removes the MPTT mixin from the StockItem model, and removes the self-referential tree structure from the database.
+
+v520 -> 2026-07-11 : https://github.com/inventree/InvenTree/pull/12310
+    - Adds new "disassemble" API endpoint for stock items
+    - Allows a stock item to be broken down into component parts, based on its Bill of Materials
 
 v519 -> 2026-07-09 : https://github.com/inventree/InvenTree/pull/TODO
     - Adds optional "roles" and "permissions" fields to the /user/me/ API endpoint, via the "?roles=true" query parameter
